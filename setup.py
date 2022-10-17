4d6696ac745431ce0e1e3578d4d71a17dfb9a498
varunbollam <varun29star@gmail.com>
2022-10-17 19:47:27 +0530

    updated get requirements file

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
 # to create packages
 from setuptools import find_packages,setup
-
+'''
 def get_requirements():
     with open("requirements.txt") as f:
-        requirements_list=[]
+        requirements_list:List[str]=[]
         for line in f:
             requirements_list.append(line.strip('\n'))
         return requirements_list
+'''
+def get_requirements():
+    """
+    Description: This function is going to return list of requirement
+    mention in requirements.txt file
+    return This function is going to return a list which contain name
+    of libraries mentioned in requirements.txt file
+    """
+    with open("requirements.txt") as requirement_file:
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        if "-e ." in requirement_list:
+            requirement_list.remove('-e .')
+        return requirement_list
 
 setup(
     name="sensor",
